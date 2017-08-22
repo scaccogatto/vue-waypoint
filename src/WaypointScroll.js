365f7abf8b6d52f3ebc154367b7570dc3db7894d
dgavagnin <dgavagnin@hce.it>
2017-08-22 18:09:53 +0200

    scroll on specified element or on window by default

--- src/WaypointScroll.js
+++ src/WaypointScroll.js
@@ -1,16 +1,32 @@
 const WaypointScroll = {
-  lastScrollX: window.pageXOffset,
-  lastScrollY: window.pageYOffset,
+  scrollElement: window,
+  lastScrollX: 0,
+  lastScrollY: 0,
   direction: {
     x: 'right',
     y: 'down'
   },
+  setScrollElement (element) {
+    WaypointScroll.scrollElement = element
+    WaypointScroll.initPosition()
+  },
+  initPosition () {
+    WaypointScroll.lastScrollX = WaypointScroll.getPositionX()
+    WaypointScroll.lastScrollY = WaypointScroll.getPositionY()
+  },
+  getPositionX () {
+    return (WaypointScroll.scrollElement !== window)
+      ? WaypointScroll.scrollElement.scrollLeft : WaypointScroll.scrollElement.pageXOffset
+  },
+  getPositionY () {
+    return (WaypointScroll.scrollElement !== window)
+      ? WaypointScroll.scrollElement.scrollTop : WaypointScroll.scrollElement.pageYOffset
+  },
   updateScrollDirection () {
-    WaypointScroll.direction.x = WaypointScroll._mapperX(Math.sign(window.pageXOffset - WaypointScroll.lastScrollX))
-    WaypointScroll.direction.y = WaypointScroll._mapperY(Math.sign(window.pageYOffset - WaypointScroll.lastScrollY))
+    WaypointScroll.direction.x = WaypointScroll._mapperX(Math.sign(WaypointScroll.getPositionX() - WaypointScroll.lastScrollX))
+    WaypointScroll.direction.y = WaypointScroll._mapperY(Math.sign(WaypointScroll.getPositionY() - WaypointScroll.lastScrollY))
 
-    WaypointScroll.lastScrollX = window.pageXOffset
-    WaypointScroll.lastScrollY = window.pageYOffset
+    WaypointScroll.initPosition()
   },
   getScrollDirection () {
     return WaypointScroll.direction
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import WaypointScroll from './WaypointScroll.js'
 import Waypoint from './Waypoint.vue'
 
 const VueWaypoint = {
+  _scrollElement: window,
+  _throttledHandler: undefined,
   install (Vue) {
     // throttle
     Vue.use(VueThrottleEvent)
@@ -12,17 +14,39 @@ const VueWaypoint = {
     // collision
     Vue.use(VueCollision)
 
-    // scroll direction handler
-    this._throttledScrollListener = VueThrottleEvent._throttle('scroll', 'v-waypoint-throttled-scroll', window)
-    window.addEventListener('v-waypoint-throttled-scroll', WaypointScroll.updateScrollDirection)
-
     // instance method
-    Vue.prototype.$scrollDirection = () => { return WaypointScroll.getScrollDirection() }
+    Vue.prototype.$scrollDirection = () => {
+      return WaypointScroll.getScrollDirection()
+    }
+
+    Vue.prototype.$unsetWaypointScrollElement = VueWaypoint._unsetWaypointScrollElement
+
+    Vue.prototype.$setWaypointScrollElement = VueWaypoint._setWaypointScrollElement
 
     // global component
     Vue.component('v-waypoint', Waypoint)
+
+    // set default scroll element to window
+    VueWaypoint._setWaypointScrollElement(VueWaypoint._scrollElement)
+  },
+  _setWaypointScrollElement (scrollElement) {
+    VueWaypoint._unsetWaypointScrollElement()
+    VueWaypoint._scrollElement = scrollElement
+    console.log('WP: adding to', VueWaypoint._scrollElement)
+    WaypointScroll.setScrollElement(VueWaypoint._scrollElement)
+    VueWaypoint._throttledHandler = VueThrottleEvent._throttle('scroll', 'v-waypoint-throttled-scroll', VueWaypoint._scrollElement)
+    VueWaypoint._scrollElement.addEventListener('v-waypoint-throttled-scroll', VueWaypoint._scrollHandler)
+  },
+  _unsetWaypointScrollElement () {
+    console.log('WP: removing from', VueWaypoint._scrollElement)
+    VueWaypoint._scrollElement.removeEventListener('v-waypoint-throttled-scroll', VueWaypoint._scrollHandler)
+    VueWaypoint._scrollElement.removeEventListener('scroll', VueWaypoint._throttledHandler)
+  },
+  _scrollHandler () {
+    WaypointScroll.updateScrollDirection()
   }
 }
+
 
 export default VueWaypoint
 
