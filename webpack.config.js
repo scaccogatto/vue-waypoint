c09c70b55aa98c05274b0996570c83d1e6dbb94b
Marco 'Gatto' Boffo <chat@marcoboffo.me>
2017-10-23 23:30:46 +0200

    :construction: Ground

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,24 @@
+const path = require('path')
 const webpack = require('webpack')
-const path    = require('path')
 
 const config = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'vue-waypoint.js',
-    library: 'VueWaypoint',
-    libraryTarget: 'umd'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'vue-waypoint.js'
   },
   module: {
     rules: [
       {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      {
         test: /\.js$/,
-        include: path.resolve(__dirname, './src'),
         exclude: /node_modules/,
-        use: "babel-loader"
+        loader: 'babel-loader'
       }
     ]
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.LoaderOptionsPlugin({ minimize: true })
+    new webpack.optimize.UglifyJsPlugin()
   ]
 }
 
-module.exports = config;
+module.exports = config
